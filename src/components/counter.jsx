5f0711f6473ef3b0c51a1d2ff5729693c0f1427d
SergeantEli <augustus.elijah@gmail.com>
2022-04-06 02:01:26 -0400

    Yup I Tottaly Put Some Effort For This Commit Name

---
+++ src/components/counter.jsx
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+//   state = {
+//     value: this.props.counter.value,
+//     tags: ["tag1", "tag2", "tag3"],
+//   };
+
+  //   constructor() {
+  //       super();
+  //       this.handleIncrement = this.handleIncrement.bind(this)
+  //   }
+
+  styles = {
+    fontSize: 20,
+    fontWeight: "bold",
+  };
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+
+//   handleIncrement = () => {
+//     console.log();
+//     this.setState({ value: this.state.value + 1 }, () => {
+//       console.log("Updated Count");
+//     });
+//   };
+
+//   doHandleIncrement = (product) => {
+//     this.handleIncrement({ id: 1 });
+//   };
+
+  render() {
+    return (
+      <div>
+        <h4>Counter #{this.props.counter.id}</h4>
+        <span style={this.styles} className={this.getBadgeClasses()}>
+          {this.formatCount()}
+        </span>
+        <button
+          onClick={() => this.props.onIncrement(this.props.counter)}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-daanger btn-sm m-2"
+        >
+          Delete
+        </button>
+        {/* {this.state.tags.length === 0 && "Please create a new tag!"}
+        {this.renderTags()} */}
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+      const {value} = this.props.counter
+    return value === 0 ? "Zero" : value;
+  }
+}
+
+export default Counter;
